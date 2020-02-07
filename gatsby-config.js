9c6833404c106af3d36ce18df825ff878925f6dc
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-02-07 17:22:52 +0100

    Use a plugin for loading roboto.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,17 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-material-ui`
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`, `700`]
+          }
+        ]
+      }
+    }
   ]
 };
