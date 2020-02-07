47702f37674bd5082f012c88247c64a43dbb6f8a
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-02-07 15:15:45 +0100

    The distance sort of works now.

--- src/components/Drawing.tsx
+++ src/components/Drawing.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Circle, Layer, Stage } from "react-konva";
+import { Circle, Layer, Stage, Line } from "react-konva";
 
 const width = 600;
 const height = 300;
@@ -23,15 +23,20 @@ const Drawing: React.FC<Props> = props => {
   console.log("scale:", scale);
 
   const scaledDist = props.distance * scale;
-  const r1x = width * 0.2 + props.r1 * scale; // TODO: calculate
-  const r2x = width * 0.8 - props.r2 * scale; // TODO: calculate
+  const r1x = width / 2 - scaledDist / 2;
+  const r2x = width / 2 + scaledDist / 2;
   console.log("r1:", r1x, "r2:", r2x);
 
   return (
     <Stage width={width} height={height}>
       <Layer clearBeforeDraw>
-        <Circle radius={props.r1 * scale} x={r1x} y={height / 2} fill="green" />
-        <Circle radius={props.r2 * scale} x={r2x} y={height / 2} fill="red" />
+        <Circle
+          radius={props.r1 * scale}
+          x={r1x}
+          y={height / 2}
+          stroke="green"
+        />
+        <Circle radius={props.r2 * scale} x={r2x} y={height / 2} stroke="red" />
       </Layer>
     </Stage>
   );
