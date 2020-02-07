40faa57e74ccb7ec4d5a4d6bb4081e9ad664c1ab
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-02-07 15:22:18 +0100

    Added lines from the center of the circles.

--- src/components/Drawing.tsx
+++ src/components/Drawing.tsx
@@ -25,16 +25,32 @@ const Drawing: React.FC<Props> = props => {
   const scaledDist = props.distance * scale;
   const r1x = width / 2 - scaledDist / 2;
   const r2x = width / 2 + scaledDist / 2;
-  console.log("r1:", r1x, "r2:", r2x);
+
+  const scaledR1 = props.r1 * scale;
+  const scaledR1x = scaledR1 * Math.cos(60);
+  const scaledR1y = scaledR1 * Math.sin(60);
+
+  const scaledR2 = props.r2 * scale;
+  const scaledR2x = scaledR2 * Math.cos(150);
+  const scaledR2y = scaledR2 * Math.sin(150);
+  console.log("SR1:", scaledR1);
 
   return (
     <Stage width={width} height={height}>
       <Layer clearBeforeDraw>
+        <Line
+          points={[r1x, height / 2, r1x + scaledR1x, height / 2 + scaledR1y]}
+          stroke="green"
+        />
         <Circle
           radius={props.r1 * scale}
           x={r1x}
           y={height / 2}
           stroke="green"
+        />
+        <Line
+          points={[r2x, height / 2, r2x + scaledR2x, height / 2 + scaledR2y]}
+          stroke="red"
         />
         <Circle radius={props.r2 * scale} x={r2x} y={height / 2} stroke="red" />
       </Layer>
